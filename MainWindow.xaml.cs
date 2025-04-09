eb0de4f4f66121e2c02f057c38f261ed7cae1255
Sufi <120452154+MarcalekM@users.noreply.github.com>
2025-04-09 10:29:50 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParalimpiaGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Paralimpia> paralimpiak = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            using StreamReader sr = new(
+                path: @"../../../src/ermek.txt",
+                encoding: Encoding.UTF8);
+            while (!sr.EndOfStream) paralimpiak.Add(new(sr.ReadLine()));
+
+            foreach (var p in paralimpiak) Paralimpiak.ItemsSource = paralimpiak;
+        }
+
+        private void Paralimpiak_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ValasztottEv.Content = paralimpiak.Where(x => x.Orszag.ToLower().Contains(Kereso.Text.ToLower()) || x.Varos.ToLower().Contains(Kereso.Text.ToLower())).ToList()[Paralimpiak.SelectedIndex].Ev;
+            ValasztottArany.Text = paralimpiak.Where(x => x.Orszag.ToLower().Contains(Kereso.Text.ToLower()) || x.Varos.ToLower().Contains(Kereso.Text.ToLower())).ToList()[Paralimpiak.SelectedIndex].Arany.ToString();
+            ValasztottEzust.Text = paralimpiak.Where(x => x.Orszag.ToLower().Contains(Kereso.Text.ToLower()) || x.Varos.ToLower().Contains(Kereso.Text.ToLower())).ToList()[Paralimpiak.SelectedIndex].Ezust.ToString();
+            ValasztottBronz.Text = paralimpiak.Where(x => x.Orszag.ToLower().Contains(Kereso.Text.ToLower()) || x.Varos.ToLower().Contains(Kereso.Text.ToLower())).ToList()[Paralimpiak.SelectedIndex].Bronz.ToString();
+        }
+
+        private void Kereso_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Kereso.Text != string.Empty) Paralimpiak.ItemsSource = paralimpiak.Where(x => x.Orszag.ToLower().Contains(Kereso.Text.ToLower()) || x.Varos.ToLower().Contains(Kereso.Text.ToLower()));
+            else Paralimpiak.ItemsSource = paralimpiak;
+        }
+    }
+}
---
+++ Paralimpia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParalimpiaGUI
+{
+    class Paralimpia
+    {
+        public int Id { get; set; }
+        public string Orszag { get; set; }
+        public string Varos { get; set; }
+        public int Ev { get; set; }
+        public int Arany { get; set; }
+        public int Ezust { get; set; }
+        public int Bronz { get; set; }
+
+        public Paralimpia(string sor)
+        {
+            var temp = sor.Split('\t');
+            Id = int.Parse(temp[0]);
+            Orszag = temp[1];
+            Varos = temp[2];
+            Ev = int.Parse(temp[3]);
+            Arany = int.Parse(temp[4]);
+            Ezust = int.Parse(temp[5]);
+            Bronz = int.Parse(temp[6]);
+        }
+    }
+}
